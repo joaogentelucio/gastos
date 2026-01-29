2913024bc7b03d327fb29999709b6bc4291a508a
João Vitor <151798489+joaogentelucio@users.noreply.github.com>
2026-01-29 16:34:15 -0300

    Implementação para pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/',
@@ -9,5 +10,34 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     }
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Minti',
+        short_name: 'MintiApp',
+        description: 'Gaste menos, viva mais!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable' 
+          }
+        ]
+      }
+    })
+  ],
 })
