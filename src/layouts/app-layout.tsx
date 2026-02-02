ac281cee6a468c112d653e30b89dc73de6d13d87
João Vitor <151798489+joaogentelucio@users.noreply.github.com>
2026-02-02 12:15:46 -0300

    ajuste no layaut

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const location = useLocation();
 
-  const noHeaderRoutes = ['/login', '/register', '/ajustes', '/perfil', 'temas', 'notificacoes'];
+  const noHeaderRoutes = ['/login', '/register', '/ajustes', '/perfil', 'temas', 'notificacoes', 'assinatura'];
   const showHeader = !noHeaderRoutes.some(route =>
     location.pathname.endsWith(route)
   );
